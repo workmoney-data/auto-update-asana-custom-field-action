0cc7d68b295dac232127f12122bc4a65d1cb75d9
Jonathan Sibley <sibljon@users.noreply.github.com>
2023-01-16 10:00:28 +0000

    merge in master to draft branches

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,64 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 import {wait} from './wait'
 
+const MainBranchName = 'main'
+
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('wait_milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ms: string = core.getInput('waitMilliseconds')
+    if (ms) {
+      core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      core.debug(new Date().toTimeString())
+      await wait(parseInt(ms, 10))
+      core.debug(new Date().toTimeString())
+    }
+
+    const githubToken: string = core.getInput('githubToken')
+    if (!githubToken) {
+      throw new Error('No github token provided')
+    }
+
+    const onlyMergeMainForDraftPullRequests = core.getBooleanInput(
+      'onlyMergeMainForDraftPullRequests'
+    )
 
     core.setOutput('time', new Date().toTimeString())
+
+    const octokit = github.getOctokit(githubToken)
+
+    const repoOwner = github.context.repo.owner
+    const repo = github.context.repo.repo
+
+    const pullRequestAbridged = github.context.payload.pull_request
+    if (pullRequestAbridged) {
+      const pullRequest = await octokit.rest.pulls.get({
+        owner: repoOwner,
+        repo,
+        pull_number: pullRequestAbridged.number
+      })
+
+      if (!onlyMergeMainForDraftPullRequests || pullRequest.data.draft) {
+        core.info(`Merging in the main branch...`)
+
+        await octokit.rest.repos.merge({
+          owner: repoOwner,
+          repo,
+          base: pullRequest.data.head.ref,
+          head: MainBranchName
+        })
+      }
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.error(error)
+      core.setFailed(error.message)
+    }
   }
 }
 
-console.log('Running Mergie...')
+core.info('Running Mergie...')
 
 run()
